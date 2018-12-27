aca566c76dedcf2a53b507c107425a3891eca925
Tpk <admin@tcgforum.pl>
2018-12-27 13:19:43 +0100

    Blocked registration for logged in, add password reset done template

--- account/views.py
+++ account/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 
 from .forms import SignUpForm
@@ -10,19 +9,22 @@ def dashboard(request):
 
 def register(request):
     
-    # TODOS: Repair register for ExtendedUser model
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('index')
+    # Blocked registration for logged in
+    if request.user.is_authenticated:
+        return redirect('index')
     else:
-        form = SignUpForm(initial={'gender': False})
+        if request.method == 'POST':
+            form = SignUpForm(request.POST)
+
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password1']
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                return redirect('index')
+        else:
+            form = SignUpForm(initial={'gender': False})
     
     context = { 'form': form }
     return render(request, 'registration/register.html', context)
