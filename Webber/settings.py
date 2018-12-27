518fbc2ef720da896782d2bf9a9e60982ca5030d
Tpk <admin@tcgforum.pl>
2018-12-27 14:29:50 +0100

    Add django-colorful package and create Category model

--- Webber/settings.py
+++ Webber/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'account',
     'blog',
+    'colorful',
     'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+import uuid
+from colorful.fields import RGBColorField
+
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+
+    identy = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    color = RGBColorField(default="#000000")
+
+    def __str__(self):
+        return self.name
