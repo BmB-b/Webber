d0c1869ee0501e06a68809610e7763851697931a
Tpk <admin@tcgforum.pl>
2018-12-29 16:17:49 +0100

    Prepare rest route and correct name

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,16 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # Index
+
+    # Display all posts
     path('', views.BlogIndex.as_view(), name='index'),
 
-    # Add
-    path('add/', views.PostCreate.as_view(), name='add'),
+    # Add new post
+    path('add/', views.PostCreate.as_view(), name='create'),
+    
+    # Update exists post
+    #path('<int:pk>/', views.PostUpdate.as_view(), name='update'),
+    
+    # Delete exists post
+    #path('<int:pk>/delete/', views.PostDelete.as_view(), name='delete'),
 ]
