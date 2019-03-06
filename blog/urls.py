4c227f2863603c2a8fb3f0e2a359cf8688f653d4
Tpk <admin@tcgforum.pl>
2019-03-06 20:18:26 +0100

    External route for Users entries in Blog app

--- blog/urls.py
+++ blog/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('add/', views.PostCreate.as_view(), name='create'),
     
     # View post detail
-    path('<slug:slug>-<int:pk>/', views.PostView.as_view(), name='view'),
+    path('<slug:slug>-<int:pk>/', views.PostView.as_view(), name='view'),    
+    path('<str:author>/<slug:slug>-<int:pk>/', views.PostView.as_view(), name='view'),
     
     # Update exists post
     path('<slug:slug>-<int:pk>/edit', views.PostEdit.as_view(), name='edit'),
