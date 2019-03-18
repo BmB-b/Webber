5d9c3bc49defd0c1c80398dbacc0e55fdcacd4b2
Tpk <admin@tcgforum.pl>
2019-03-18 22:09:11 +0100

    Posts admin section with translations

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Category, Post
 
+# Actions
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'state', 'pub_date']
+    ordering = ['title']
+    actions = ['make_published','make_unpublished','make_draft']
+
+    def make_draft(self, request, queryset):
+        queryset.update(state=1)
+    make_draft.short_description = _('admin_post_state_draft')
+
+    def make_published(self, request, queryset):
+        queryset.update(state=2)
+    make_published.short_description = _('admin_post_state_publish')
+
+    def make_unpublished(self, request, queryset):
+        queryset.update(state=3)
+    make_unpublished.short_description = _('admin_post_state_unpublish')
+
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
